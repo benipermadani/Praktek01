083acec5c339f23853195587f9e49374b8840d4b
Beni Permadani <94419695+benipermadani@users.noreply.github.com>
2022-12-01 11:01:42 +0800

    Mengisi nilai atribut

--- src/praktek01/MahasiswaAksi.java
+++ src/praktek01/MahasiswaAksi.java
@@ -12,5 +12,9 @@ package praktek01;
 public class MahasiswaAksi {
     public static void main(String[] args) {
         Mahasiswa Beni = new Mahasiswa();
+        
+        Beni.nama= " Beni permadani";
+        Beni.NPM= " 2010020029";;
+        Beni.IPK= " 3.0"
     }
 }
